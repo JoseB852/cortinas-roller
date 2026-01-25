bbb85e77fe4f9e26e612645a88f190f656b6c322
JoseB852 <jbetancourts976@gmail.com>
2026-01-25 20:53:27 -0300

    Fix App.js cleanup and improve footer behavior

--- src/App.js
+++ src/App.js
@@ -2,25 +2,28 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 import Nabvar from './components/Nabvar/Nabvar';
-import Home from './views/Home';
 import Footer from './components/Nabvar/Footer/Footer';
-import Comercial from './views/Comercial/Comercial';
-import Card from './views/Card/Card';
 import SectionInfo from './components/Nabvar/SectionInfo/SectionInfo';
 import Whatsapp from './components/Nabvar/Whatsapp/Whatsapp';
+
+import Home from './views/Home';
+import Card from './views/Card/Card';
+import Comercial from './views/Comercial/Comercial';
 import ProductoDetalle from './views/ProductoDetalle/ProductoDetalle';
 
-function App() {
+export default function App() {
   return (
     <BrowserRouter>
       <Nabvar />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/card" element={<Card />} />
-        <Route path="/comercial" element={<Comercial />} />
-        <Route path="/productoDetalle/:id" element={<ProductoDetalle />} />
-      </Routes>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/card" element={<Card />} />
+          <Route path="/comercial" element={<Comercial />} />
+          <Route path="/productoDetalle/:id" element={<ProductoDetalle />} />
+        </Routes>
+      </main>
 
       <SectionInfo />
       <Whatsapp />
@@ -28,5 +31,3 @@ function App() {
     </BrowserRouter>
   );
 }
-
-export default App;
