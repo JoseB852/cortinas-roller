9be9940dfa71e1b64c9a232b86faa204fec782b0
JoseB852 <jbetancourts976@gmail.com>
2026-01-25 20:45:26 -0300

    Fix ESLint errors and successful production build

--- src/App.js
+++ src/App.js
@@ -1,43 +1,32 @@
-import { BrowserRouter,Routes, Route, RouterProvider } from 'react-router-dom';
-import { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+
 import Nabvar from './components/Nabvar/Nabvar';
 import Home from './views/Home';
 import Footer from './components/Nabvar/Footer/Footer';
 import Comercial from './views/Comercial/Comercial';
-
 import Card from './views/Card/Card';
 import SectionInfo from './components/Nabvar/SectionInfo/SectionInfo';
 import Whatsapp from './components/Nabvar/Whatsapp/Whatsapp';
 import ProductoDetalle from './views/ProductoDetalle/ProductoDetalle';
 
-
-
-
-
-
-
-
-
 function App() {
   return (
-    
-  <>
-  <BrowserRouter>
-  <Nabvar/>
-  <Routes>
-    <Route path='/' element={<Home/>}/>
-    <Route path='/card' element={<Card/>} />
-    <Route path= "/comercial" element={<Comercial/>}/>
-    <Route path='/productoDetalle/:id' element={<ProductoDetalle/>}/>
-  </Routes>
-  <SectionInfo/>
-  <Whatsapp/>
-  <Footer />
-  </BrowserRouter>
-  
-  </>
-)
+    <BrowserRouter>
+      <Nabvar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/card" element={<Card />} />
+        <Route path="/comercial" element={<Comercial />} />
+        <Route path="/productoDetalle/:id" element={<ProductoDetalle />} />
+      </Routes>
+
+      <SectionInfo />
+      <Whatsapp />
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
