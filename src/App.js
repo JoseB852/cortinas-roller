43d21609b35688542848bdb0909dee0280b65f8f
JoseB852 <jbetancourts976@gmail.com>
2026-02-04 10:04:38 -0300

    actualizacion

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-
+import './index.css';
 import Nabvar from './components/Nabvar/Nabvar';
 import Footer from './components/Nabvar/Footer/Footer';
 import SectionInfo from './components/Nabvar/SectionInfo/SectionInfo';
