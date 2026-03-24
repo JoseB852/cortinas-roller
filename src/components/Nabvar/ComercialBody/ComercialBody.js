188450b74d49ddab5e943ac93acefc0c3e3bede4
JoseB852 <jbetancourts976@gmail.com>
2026-03-24 18:11:36 -0300

    comercial

--- src/components/Nabvar/ComercialBody/ComercialBody.js
+++ src/components/Nabvar/ComercialBody/ComercialBody.js
@@ -1,9 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import './ComercialBody.css'
+import { FaDollarSign, FaSun, FaCheckCircle, FaShieldAlt } from "react-icons/fa";
 
 export default function ComercialBody() {
 
     const [body, setBody] = useState([]);
+
+    const iconMap = {
+        dollar: <FaDollarSign />,
+        sun: <FaSun />,
+        check: <FaCheckCircle />,
+        shield: <FaShieldAlt />
+    };
 
     const getBody = () => {
         return fetch('/data/body.json', {
@@ -34,20 +42,23 @@ export default function ComercialBody() {
     return (
         <>
             <h1>DISEÑADO EXCLUSIVAMENTE PARA TI</h1>
+
             <div className='comercial-body'>
                 {
                     body.map((content) => (
                         <div className='body-one' key={content.id}>
+
+                            <div className="icon">
+                                {iconMap[content.icon]}
+                            </div>
+
                             <h1>{content.titulo}</h1>
                             <p>{content.text}</p>
+
                         </div>
-
-
                     ))
                 }
-
             </div>
         </>
     )
-
 }
