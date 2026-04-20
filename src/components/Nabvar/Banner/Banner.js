43c004ab1b89bebbbe38c540a3804426603c3669
JoseB852 <jbetancourts976@gmail.com>
2026-04-20 14:16:27 -0400

    cambios

--- src/components/Nabvar/Banner/Banner.js
+++ src/components/Nabvar/Banner/Banner.js
@@ -1,32 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./Banner.css";
 
-const images = [
-  "/images/banner-medidas.png",
-  "/images/banner-medidas2.png",
-];
-
 export default function Banner() {
-  const [index, setIndex] = useState(0);
-
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setIndex((prev) => (prev + 1) % images.length);
-    }, 4000);
-
-    return () => clearTimeout(timeout);
-  }, [index]);
-
   return (
     <div className="container-banner">
-      {images.map((img, i) => (
-        <img
-          key={i}
-          src={img}
-          className={`slide ${i === index ? "active" : ""}`}
-          alt="banner"
-        />
-      ))}
+      <img
+        src="/images/banner-medicion.jpg"
+        alt="banner"
+        className="banner-img"
+      />
     </div>
   );
 }
