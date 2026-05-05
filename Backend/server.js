8f1daca6d728f9dd783816a682349db29e27636e
JoseB852 <jbetancourts976@gmail.com>
2026-05-05 17:05:38 -0400

    cambios

--- Backend/server.js
+++ Backend/server.js
@@ -3,18 +3,13 @@ const express = require("express")
 const cors = require("cors")
 
 const emailRoutes = require("./routes/email");
-const iaRoutes = require("./routes/ia"); 
 const manejadorErrores = require("./middlewares/manejadorErorres")
 
 
 const app = express()
 app.use(express.json())
 app.use(cors())
-app.use(express.static("public"));
-
-
 app.use("/email", emailRoutes)
-app.use("/ia", iaRoutes); 
 app.use(manejadorErrores)
 
 const PORT = process.env.PORT || 3000
